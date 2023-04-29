594301e51b3226ee8877ca14300ae2d4cd5714f2
Ales Zelinka <azelinka@redhat.com>
2023-04-29 21:58:46 +0200

    rename co2 to voc as that's what the sensor actually measures

--- air_quality_sensor.ino
+++ air_quality_sensor.ino
@@ -7,12 +7,12 @@
 /* The MQ135 library need calibration based on clean atmospheric air
 * 1. comment out the upload() call and decrease sleep time to few seconds
 * 2. put the sensor outside where air is not polluted for few minutes
-*    until the CO2 rzero value stabilizes
+*    until the voc rzero value stabilizes
 * 3. put the rzero value to MQ135.h libarary to RZERO macro
 * 4. uncoment upload() and set sleep to final value
 * 5. recompile and reupload firmware to esp8266
 *
-* When rzero is set correctly, the corrected CO2 should be around 400 in clean air
+* When rzero is set correctly, the corrected voc should be around 400 in clean air
 */
 
 // DHT sensor config, used to calibrate MQ135
@@ -27,7 +27,7 @@ MQ135 mq135 = MQ135(MQ135_PIN);
 // sensors' values
 float temperature;
 float humidity;
-float corrected_co2;
+float corrected_voc;
 float rzero;
 
 void setup() {
@@ -43,16 +43,16 @@ void loop() {
   //get sensors data
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
-  float co2 = mq135.getPPM();
-  corrected_co2 = mq135.getCorrectedPPM(temperature, humidity);
+  float voc = mq135.getPPM();
+  corrected_voc = mq135.getCorrectedPPM(temperature, humidity);
   rzero = mq135.getRZero();
 
   // process all data
   logln("Temperature: "+ String(temperature));
   logln("Humidity: " + String(humidity));
-  logln("CO2: "+ String(co2));
-  logln("CO2 corrected: "+ String(corrected_co2));
-  logln("CO2 rzero: "+ String(rzero));
+  logln("VOC: "+ String(voc));
+  logln("VOC corrected: "+ String(corrected_voc));
+  logln("VOC rzero: "+ String(rzero));
 
   wifi_reconnect(ssid, wifi_password, ip_last_byte);
   upload();
@@ -63,13 +63,13 @@ void loop() {
 void upload() {
   Broker b = Broker(broker_url);
   b.addProperty("refresh_rate", String(refresh_rate));
-  // report temp& humidity as debug - they are meant to be used for co2
+  // report temp& humidity as debug - they are meant to be used for voc
   // correction only, not ambient meassurements as they are affected by 
   // the mq135 sensor's heat
   // report only 1 decimal place - saves space in HA dashboards
   b.addProperty("debug_temperature", String(temperature, 1));
   b.addProperty("debug_humidity", String(humidity, 1));
-  b.addProperty("co2", String(corrected_co2, 1));
+  b.addProperty("voc", String(corrected_voc, 1));
   b.addProperty("debug_rzero", String(rzero, 1));
   b.upload();
 }
